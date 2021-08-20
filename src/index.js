024c1a43ba8129c2ce0588329c60d42a31697263
ldh7228 <ldh7228@gmail.com>
2021-08-20 19:15:20 +0900

    Fix card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const main = document.getElementById('main');
 
 let cardArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-let num = 0;
 let n = 0;
 let card = [];
 let randomNumber;
+let num = 0;
+let number = [];
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -23,8 +24,7 @@ for (let i = 0; i < 2; i++) {
     for(let j = 0; j < 10; j++) {
             main.children[i].children[j].addEventListener('click', function() {
             main.children[i].children[j].children[0].src = " ";
-            main.children[i].children[j].children[0].src = `../asset/img/${card[num]}.png`;
-            num++;
+            main.children[i].children[j].children[0].src = `../asset/img/${card[(i * 10) + j]}.png`;
         });
     }
 }
